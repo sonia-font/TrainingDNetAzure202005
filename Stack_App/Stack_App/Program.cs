9967a217e75af359721b2198b6c246b06adc1079
sonia-font <sonia.font91@gmail.com>
2020-05-20 14:35:54 -0300

    fixed

--- Stack_App/Stack_App/Program.cs
+++ Stack_App/Stack_App/Program.cs
@@ -9,110 +9,49 @@ namespace Stack_App
         static void Main(string[] args)
         {
             int size;
-            int option;
-            int subOption1;
-            int subOption2;
+            int option;                        
             string name;
             string lastname;
-            int number;                       
+                                  
 
             Console.WriteLine("Hi!\nChoose a collection size");
             size = Convert.ToInt32(Console.ReadLine());            
             Console.WriteLine("Max size " + size);
             Stack<Student> theStack = new Stack<Student>();
             do
-            {
-                Console.WriteLine("Choose an option:\n1. Push or Pop operations\n2. Show items\n3. Exit");
+            {                
+                Console.WriteLine("Choose an option:\n1. Push\n2. Pop\n3. Exit");
                 option = Convert.ToInt32(Console.ReadLine());
-
                 if (option != 3)
                 {
                     if (option == 1)
                     {
-                        Console.WriteLine("Choose an option:\n1. Push\n2. Pop\n3. Back");
-                        subOption1 = Convert.ToInt32(Console.ReadLine());
-                        if (subOption1 != 3)
+                        try
                         {
-                            if (subOption1 == 1)
-                            {
-                                try
-                                {
-                                    Console.WriteLine("Student name: ");
-                                    name = Console.ReadLine();
-                                    Console.WriteLine("Student lastname: ");
-                                    lastname = Console.ReadLine();
-                                    theStack.Push(new Student() { FirstName = name, LastName = lastname });
-                                }
-                                catch (Exception)
-                                {
-                                    throw new MyStackOverflowException("Stack full");
-                                }
-                            }
-                            else
-                            {
-                                try
-                                {
-                                    theStack.Pop();
-                                }
-                                catch (Exception)
-                                {
-                                    throw new MyStackUnderFlowException("Stack is empty");
-                                }
-                            }
+                            Console.WriteLine("Student name: ");
+                            name = Console.ReadLine();
+                            Console.WriteLine("Student lastname: ");
+                            lastname = Console.ReadLine();
+                            theStack.Push(new Student() { FirstName = name, LastName = lastname });
+                        }
+                        catch (Exception)
+                        {
+                            throw new MyStackOverflowException("Stack full");
                         }
                     }
                     else
                     {
-                        Console.WriteLine("Introduce desired option:\n1. Default\n2. TopBottom\n3. BottomTop\n4. TopN\n5. Back");
-                        subOption2 = Convert.ToInt32(Console.ReadLine());
-                        switch (subOption2)
+                        try
                         {
-                            case 1:
-                                {
-                                    foreach (Student student in theStack)
-                                    {
-                                        student.GetName();
-                                    }
-                                    Console.WriteLine();
-                                    break;
-                                }
-                            case 2:
-                                {
-                                    foreach (Student student in theStack.TopToBottom)
-                                    {
-                                        student.GetName();
-                                    }
-                                    Console.WriteLine();
-                                    break;
-                                }
-                            case 3:
-                                {
-                                    foreach (Student student in theStack.BottomToTop)
-                                    {
-                                        student.GetName(); 
-                                    }
-                                    Console.WriteLine();
-                                    break;
-                                }
-                            case 4:
-                                {
-                                    Console.WriteLine("How many elements?");
-                                    number = Convert.ToInt32(Console.ReadLine());
-                                    foreach (Student student in theStack.TopN(number))
-                                    {
-                                        student.GetName();
-                                    }
-                                    Console.WriteLine();
-                                    break;
-                                }
-                            case 5:
-                                {
-                                    break;
-                                }
+                            theStack.Pop();
+                        }
+                        catch (Exception)
+                        {
+                            throw new MyStackUnderFlowException("Stack is empty");
                         }
                     }
-
-                }
+                }                
+                
             } while (option != 3); 
             
             Console.ReadKey();            
--- Stack_App/Stack_App/Stack.cs
+++ Stack_App/Stack_App/Stack.cs
@@ -7,10 +7,10 @@ using System.Text;
 
 namespace Stack_App
 {
-    public class Stack<T> : IEnumerable<T>
+    public class Stack<T> 
     {
         public static int _size;
-        private T[] values = new T[10];
+        private T[] values = new T[2];
         private int top = 0;
 
         /*public Stack(int size)
@@ -27,49 +27,6 @@ namespace Stack_App
         {
             top--;
             return values[top];
-        }
-
-        // This method implements the GetEnumerator method. It allows
-        // an instance of the class to be used in a foreach statement.
-        public IEnumerator<T> GetEnumerator()
-        {
-            for (int index = top - 1; index >= 0; index--)
-            {
-                yield return values[index];
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public IEnumerable<T> TopToBottom
-        {
-            get { return this; }
-        }
-
-        public IEnumerable<T> BottomToTop
-        {
-            get
-            {
-                for (int index = 0; index <= top - 1; index++)
-                {
-                    yield return values[index];
-                }
-            }
-        }
-
-        public IEnumerable<T> TopN(int itemsFromTop)
-        {
-            // Return less than itemsFromTop if necessary.
-            int startIndex = itemsFromTop >= top ? 0 : top - itemsFromTop;
-
-            for (int index = top - 1; index >= startIndex; index--)
-            {
-                yield return values[index];
-            }
-        }
-
+        }        
     }
 }
