c548bc4281fe8a384007b494ae72cf11165d8869
sonia-font <sonia.font91@gmail.com>
2020-06-18 20:29:08 -0300

    updated docker

--- NetCoreTraining/Training.Presentation.API/Program.cs
+++ NetCoreTraining/Training.Presentation.API/Program.cs
@@ -6,6 +6,11 @@ using Serilog.Formatting;
 using Serilog.Formatting.Compact;
 using Serilog.Formatting.Json;
 using Serilog.Formatting.Display;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Reflection;
 
 
 namespace Training.Presentation.API
@@ -14,18 +19,71 @@ namespace Training.Presentation.API
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+			//configure logging first
+			ConfigureLogging();
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-        {
-            return WebHost.CreateDefaultBuilder(args).UseSerilog((builderContext, configuration) => {
-                configuration.WriteTo.File(new MessageTemplateTextFormatter("[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"), "logs/log.json",Serilog.Events.LogEventLevel.Information);
-                configuration.MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning);
-                configuration.MinimumLevel.Override("Microsoft.EntityFrameworkCore", Serilog.Events.LogEventLevel.Warning);
-                configuration.WriteTo.Console();
-            }).UseStartup<Startup>();
-        }
-    }
+			//then create the host, so that if the host fails we can log errors
+			CreateHost(args);
+
+		}
+
+		private static void CreateHost(string[] args)
+		{
+			try
+			{
+				CreateHostBuilder(args).Build().Run();
+			}
+			catch (System.Exception ex)
+			{
+				Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
+				throw;
+			}
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				})
+				.ConfigureAppConfiguration(configuration =>
+				{
+					configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+					configuration.AddJsonFile(
+						$"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+						optional: true);
+				})
+				.UseSerilog();
+
+		private static void ConfigureLogging()
+		{
+			var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+			var configuration = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.AddJsonFile(
+					$"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+					optional: true)
+				.Build();
+
+			Log.Logger = new LoggerConfiguration()
+				.Enrich.FromLogContext()
+				.Enrich.WithMachineName()
+				.WriteTo.Debug()
+				.WriteTo.Console()
+				.WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+				.Enrich.WithProperty("Environment", environment)
+				.ReadFrom.Configuration(configuration)
+				.CreateLogger();
+		}
+
+		private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+		{
+			return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+			{
+				AutoRegisterTemplate = true,
+				IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+			};
+		}
+	}
 
 }
